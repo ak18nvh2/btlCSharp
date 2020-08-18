ead08bc80128845170dab3d06ceffc090d2b713c
ak18nvh2 <50037709+ak18nvh2@users.noreply.github.com>
2020-08-18 15:48:54 +0700

    nug

--- DTOs/InventoryDTO.cs
+++ DTOs/InventoryDTO.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace DTOs
 {
-    public class Class1
+    public class InventoryDTO: IComparable<InventoryDTO>
     {
+        public string PartName { get; set; }
+        public string TransactionType { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public double Amount { get; set; }
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public InventoryDTO() { }
+        public InventoryDTO(string pn, string tt, DateTime td, double a, string s, string d)
+        {
+            this.PartName = pn;
+            this.TransactionType = tt;
+            this.TransactionDate = td;
+            this.Amount = a;
+            this.Source = s;
+            this.Destination = d;
+        }
+
+        public int CompareTo(InventoryDTO other)
+        {
+           if(this.TransactionDate > other.TransactionDate)
+            {
+                return 1;
+            }
+           else if (this.TransactionDate == other.TransactionDate)
+            {
+                if (this.TransactionType != "Purchase Order")
+                    return 1;
+            }
+            return 0;
+        }
     }
 }
